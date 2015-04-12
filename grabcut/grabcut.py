785f53c3f45af80afd9195e40499c34724381ea6
Jasper Lin <Jasper@rescomp-14-301093.stanford.edu>
2015-04-11 20:11:01 -0700

    implementeded k means

--- grabcut/grabcut.py
+++ grabcut/grabcut.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pdb
 from scipy.misc import imread
-
+import random
+import scipy
 def grabcut(image_file, box=None):
     print 'LOADING IMAGE FROM FILE: {}'.format(image_file)
     img = imread(image_file)
@@ -15,10 +16,12 @@ def grabcut(image_file, box=None):
 
     img = preprocess(img)
 
-    # Initialize segmentation map to bounding box
+    # Initialize segmentation map to bounding box, foreground = 1, background = 0
     seg_map = np.zeros((img.shape[0], img.shape[1]))
     seg_map[(box['y_min'] + 1):box['y_max'], (box['x_min'] + 1):box['x_max']] = 1
-
+    # initialize components with k-means, from lecture notes
+    initial = k_means(img,box)
+    scipy.misc.imsave('/Users/Jasper/Desktop/test.png',initial)
     gmm = fit_gmm(img, seg_map)
 
     return seg_map
@@ -48,6 +51,59 @@ def select_bounding_box(img):
     plt.show()
 
     return box
+
+def cluster_points(X, mu):
+    clusters = {}
+    for x in X:
+        color = x[0]
+        dist = [ ]
+        for i in mu:
+            dist.append((np.linalg.norm(color-i),i))
+        bestmukey = min(dist, key=lambda t:t[0])[1]
+        try:
+            clusters[tuple(bestmukey)].append(x)
+        except KeyError:
+            clusters[tuple(bestmukey)] = [x]
+    return clusters
+
+def reevaluate_centers(clusters):
+    newmu = [ ]
+    for k in clusters:
+        total = np.zeros((1,3))
+        for a in clusters[k]:
+            total += a[0]
+        total /= len(clusters[k])
+        newmu.append(total[0])
+    return newmu
+
+def has_converged(mu, oldmu):
+    return set([tuple(a) for a in mu]) == set([tuple(a) for a in oldmu])
+
+def k_means(img, box, k=5):
+    y_min = box['y_min']
+    y_max = box['y_max']
+    x_min = box['x_min']+1
+    x_max = box['x_max']
+    colorList = [ ]
+    for y in range(int(y_min),int(y_max)):
+        for x in range(int(x_min),int(x_max)):
+            colorList.append((img[y,x,:],(y,x)))
+    oldmu = [a[0] for a in random.sample(colorList,k)]
+    mu = [a[0] for a in random.sample(colorList,k)]
+    counter = 1
+    while not has_converged(mu, oldmu):
+        print counter
+        counter += 1
+        oldmu = mu
+        #assign points to clusters
+        clusters = cluster_points(colorList, mu)
+        #reevaluate centers
+        mu = reevaluate_centers(clusters)
+    initial = np.zeros(img.shape)
+    for i,j in enumerate(clusters.keys()):
+        for k in clusters[j]:
+            initial[k[1]] = (i+1)*30
+    return initial
 
 def fit_gmm(img, seg_map, k=5):
     pass
