fc61143a621f74dfc91ce5be08b3949776efa9e5
Dylan Rhodes <dylanr@stanford.edu>
2015-04-18 22:14:10 -0700

    Implement remaining functionality

--- grabcut/grabcut.py
+++ grabcut/grabcut.py
@@ -1,13 +1,12 @@
 import matplotlib.pyplot as plt
 import maxflow
 import numpy as np
+from os import listdir
 import pdb
 import random
-import scipy
-from os import listdir
-from scipy.misc import imread
-from scipy.misc import imsave
+from scipy.misc import imread, imsave
 from scipy.spatial.distance import cdist
+import time
 
 
 KMEANS_CONVERGENCE = 1.0
@@ -23,28 +22,57 @@ STRUCTURES = {
     'LEFT': np.array([[0, 0, 0], [1, 0, 0], [0, 0, 0]]),
     'RIGHT': np.array([[0, 0, 0], [0, 0, 1], [0, 0, 0]]),
 }
-def runAlgorithm( ):
-    bboxes = [f for f in listdir('./bboxes')]
-    data = [f for f in listdir('./data')]
-    ground_truth = [f for f in listdir('./ground_truth')]
+
+def test_grabcut():
+    box_list = sorted(['./bboxes/' + f for f in listdir('./bboxes')])
+    data_list = sorted(['./data/' + f for f in listdir('./data')])
+    truth_list = sorted(['./ground_truth/' + f for f in listdir('./ground_truth')])
+
     output = open('./results.txt','wb')
-    for box, img, truth in zip(bboxes,data,ground_truth):
-        f = open('./bboxes/'+box) 
-        bounds = f.readline().split()
-        bounding = { }
-        bounding['xmin'] = int(bounds[0])
-        bounding['ymin'] = int(bounds[1])
-        bounding['xmax'] = int(bounds[2])
-        bounding['ymax'] = int(bounds[3])
-        image = imread('./data/'+img)
-        gtruth = imread('./ground_truth/'+truth)
-        seg = grabcut(image, bounding)
-        similarity = jaccard_similarity(seg, gtruth)
-        imsave('./output/'+img,np.where(seg==0,0,255))
-        output.write(box+'\t'+similarity+'\n')
+    mean_accuracy = 0.0
+
+    for box_path, img_path, truth_path in zip(box_list, data_list, truth_list):
+        with open(box_path) as box_file:
+            bounds = box_file.readline().split()
+
+            box = {
+                'x_min': int(bounds[0]),
+                'y_min': int(bounds[1]),
+                'x_max': int(bounds[2]),
+                'y_max': int(bounds[3])
+            }
+
+        print 'SEGMENTING IMAGE {}'.format(img_path)
+        img = imread(img_path)
+
+        ground_truth = imread(truth_path)
+        ground_truth[ground_truth > 0] = 1
+
+        start = time.time()
+        seg = grabcut(img, box)
+        end = time.time()
+        print 'SEGMENTATION TOOK {} SECONDS FOR {} x {} IMAGE'.format(end - start, img.shape[1], img.shape[0])
+
+        accuracy = get_accuracy(seg, ground_truth)
+        mean_accuracy += accuracy
+        
+        similarity = 0.0
+        #similarity = jaccard_similarity(seg, ground_truth)
+
+        print 'OBTAINED FINAL ACCURACY: {}, JACCARD SIMILARITY: {}'.format(accuracy, similarity)
+        imsave('./output/' + img_path.split('/')[2], np.where(seg == 0, 0, 255))
+        output.write('{}\t{}\n'.format(box_path, similarity))
+
+    mean_accuracy /= len(data_list)
+    print 'MEAN ACCURACY: {}'.format(mean_accuracy)
+
     output.close()
-def grabcut(image_file, box=None):
-    img = image_file
+
+def get_accuracy(seg_map, ground_truth):
+    correct = (seg_map + ground_truth - 1) ** 2
+    return np.mean(correct)
+
+def grabcut(img, box=None):
     # Allow user to select bounding box if not provided
     if box is None:
         box = select_bounding_box(img)
@@ -65,17 +93,18 @@ def grabcut(image_file, box=None):
     # Iteratively refine segmentation from initialization
     for i in xrange(MAX_NUM_ITERATIONS):
         fg_gmm, bg_gmm = fit_gmm(fg, bg, fg_ass, bg_ass)
-        seg_map, fg_ass, bg_ass = estimate_segmentation(img, fg_gmm, bg_gmm, seg_map)
-
+        seg_map, fg_ass, bg_ass = estimate_segmentation(img, fg_gmm, bg_gmm, seg_map, box)
         fg = img[seg_map == 1]
         bg = img[seg_map == 0]
+
+        print 'AT ITERATION {}, {} FOREGROUND / {} BACKGROUND'.format(i + 1, fg.shape[0], bg.shape[0])
 
     return seg_map
 
 def preprocess(img):
     return img
 
-def estimate_segmentation(img, fg_gmm, bg_gmm, seg_map):
+def estimate_segmentation(img, fg_gmm, bg_gmm, seg_map, box):
     # Calculate unary values and component assignments
     fg_unary, fg_ass = get_unary(img, fg_gmm)
     bg_unary, bg_ass = get_unary(img, bg_gmm)
@@ -83,14 +112,35 @@ def estimate_segmentation(img, fg_gmm, bg_gmm, seg_map):
     # Calculate pairwise values
     pair_pot = get_pairwise(img)
 
+    # Remove portion outside bounding box
+    fg_unary = fg_unary[box['y_min']:box['y_max'], box['x_min']:box['x_max']]
+    bg_unary = bg_unary[box['y_min']:box['y_max'], box['x_min']:box['x_max']]
+    pair_pot = pair_pot[:, box['y_min']:box['y_max'], box['x_min']:box['x_max']]
+
     # Construct graph and run mincut on it
     pot_graph, nodes = create_graph(fg_unary, bg_unary, pair_pot)
     pot_graph.maxflow()
 
     # Create bit map of result and return it
-    seg_map = segment(pot_graph, nodes)
+    box_seg = segment(pot_graph, nodes)
+    
+    seg_map = np.zeros((img.shape[0], img.shape[1]), dtype='int32')
+    seg_map[box['y_min']:box['y_max'], box['x_min']:box['x_max']] = box_seg
 
-    return seg_map, fg_ass, bg_ass
+    return seg_map, fg_ass[seg_map == 1], bg_ass[seg_map == 0]
+
+def adjust_outside_box(fg_unary, bg_unary, box):
+    fg_unary[:box['y_min'], :] = -100000
+    bg_unary[:box['y_min'], :] = 100000
+
+    fg_unary[:, :box['x_min']] = -100000
+    bg_unary[:, :box['x_min']] = 100000
+
+    fg_unary[box['y_max']:, :] = -100000
+    bg_unary[box['y_max']:, :] = 100000
+
+    fg_unary[:, box['x_max']:] = -100000
+    bg_unary[:, box['x_max']:] = 100000
 
 def create_graph(fg_unary, bg_unary, pair_pot):
     graph = maxflow.Graph[float]()
@@ -107,7 +157,7 @@ def create_graph(fg_unary, bg_unary, pair_pot):
 
 def segment(graph, nodes):
     segments = graph.get_grid_segments(nodes)
-    return np.int_(np.logical_not(segments))
+    return (np.int_(np.logical_not(segments)) - 1) * -1
 
 def get_pairwise(img):
     H, W, C = img.shape
@@ -153,10 +203,13 @@ def get_unary(img, gmm):
     H, W, C = img.shape
 
     potentials = np.zeros((K, H, W))
+    log_pdfs = np.zeros((K, H, W), dtype='float64')
 
     for k, gaussian in enumerate(gmm):
         cov = gaussian['cov']
         mu_img = img - np.reshape(gaussian['mean'], (1, 1, 3))
+
+        log_pdfs[k] += np.log(1.0 / np.sqrt(2 * (np.pi ** 3) * np.linalg.det(cov)))
 
         piece1 = -np.log(gaussian['size']) + 0.5 * np.log(np.linalg.det(cov))
         temp = np.einsum('ijk,il', np.transpose(mu_img), np.linalg.inv(cov))
@@ -168,9 +221,15 @@ def get_unary(img, gmm):
                 piece2[i, j] = np.dot(temp[j, i], mu_img[i, j])
         
         potentials[k] = piece1 + piece2
+        log_pdfs[k] += -1.0 * piece2
     
-    unary = np.max(np.array(potentials), axis=0)
-    assignments = np.argmax(np.array(potentials), axis=0)
+    try:
+        unary = np.max(np.array(potentials), axis=0)
+        assignments = np.argmax(np.array(log_pdfs), axis=0)
+    except:
+        pdb.set_trace()
+
+    print 'CALCULATING UNARY POTENTIALS...'
 
     return unary, assignments
 
@@ -197,7 +256,7 @@ def quick_k_means(foreground, background, k=5):
         avg_centroid_change += np.sqrt(np.sum(np.square(fg_mu - new_fg_mu), axis=1))
         avg_centroid_change = np.mean(avg_centroid_change) / 2
 
-        print "AVERAGE CENTROID CHANGE: {}".format(avg_centroid_change)
+        # print "AVERAGE CENTROID CHANGE: {}".format(avg_centroid_change)
 
         fg_mu = new_fg_mu
         bg_mu = new_bg_mu
@@ -207,11 +266,12 @@ def quick_k_means(foreground, background, k=5):
 def fit_gmm(fg, bg, fg_ass, bg_ass, k=5):
     fg_gmms, bg_gmms = [], []
 
-#    pdb.set_trace()
-
     for i in xrange(k):
         fg_cluster = fg[fg_ass == i]
         bg_cluster = bg[bg_ass == i]
+
+        # print 'FG CLUSTER SIZE: {}'.format(fg_cluster.shape[0])
+        # print 'BG CLUSTER SIZE: {}'.format(bg_cluster.shape[0])
 
         fg_gmm, bg_gmm = {}, {}
 
@@ -224,8 +284,12 @@ def fit_gmm(fg, bg, fg_ass, bg_ass, k=5):
         fg_gmm['size'] = fg_cluster.shape[0] * 1.0 / fg.shape[0]
         bg_gmm['size'] = bg_cluster.shape[0] * 1.0 / bg.shape[0]
 
-        fg_gmms.append(fg_gmm)
-        bg_gmms.append(bg_gmm)
+        if fg_gmm['size'] > 0.001:
+            fg_gmms.append(fg_gmm)
+        if bg_gmm['size'] > 0.001:
+            bg_gmms.append(bg_gmm)
+
+    print 'FITTING GMM...'
 
     return fg_gmms, bg_gmms
 
@@ -265,4 +329,4 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         grabcut(sys.argv[1])
     else:
-        runAlgorithm()
+        test_grabcut()
